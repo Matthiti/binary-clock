685737fa6b2f945612c1df67e2e385c7ff66bc65
Matthijs Roelink <matthijs@roelink.eu>
2020-11-08 15:26:22 +0100

    Implemented minutes and clock ticking

--- binary_clock.ino
+++ binary_clock.ino
@@ -24,10 +24,20 @@ bool inSetHourMode = false;
 bool inSetMinuteMode = false;
 
 int hour = 0;
+int minute = 0;
+
+long prevMillis = 0;
 
 void setup() {
   pinMode(setButton, INPUT);
   pinMode(changeButton, INPUT);
+
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
 
   pinMode(led9, OUTPUT);
   pinMode(led10, OUTPUT);
@@ -42,9 +52,11 @@ void loop() {
     if (inSetHourMode) {
       inSetHourMode = false;
       inSetMinuteMode = true;
-      // TODO: set minutes
+      showMinuteActive();
     } else if (inSetMinuteMode) {
       inSetMinuteMode = false;
+      prevMillis = millis() / 60000 * 60000;
+      showSetDone();
     } else {
       inSetHourMode = true;
       showHourActive();
@@ -59,36 +71,67 @@ void loop() {
     if (inSetHourMode) {
       hour = (hour + 1) % 24;
       writeHour();
-    } else {
-      
+    } else if (inSetMinuteMode) {
+      minute = (minute + 1) % 60;
+      writeMinute();
     }
     changeButtonLow = false;
   } else if (changeState == LOW) {
     changeButtonLow = true;
   }
+
+  if (!inSetMode()) {
+    long currMillis = millis();
+    if (currMillis - prevMillis >= 60000) {
+      prevMillis = currMillis / 60000 * 60000;
+      minute = (minute + 1) % 60;
+      writeMinute();
+
+      if (minute == 0) {
+        hour = (hour + 1) % 24;
+        writeHour();
+      }
+    }
+  }
+}
+
+boolean inSetMode() {
+  return inSetMinuteMode || inSetHourMode;
 }
 
 void writeHour() {
-  writeTime(led9, led13);
+  writeTime(hour, led9, led13);
 }
 
 void writeMinute() {
-  writeTime(led3, led8);
+  writeTime(minute, led3, led8);
 }
 
-void writeTime(int ledStart, int ledEnd) {
+void writeTime(int number, int ledStart, int ledEnd) {
   for (int led = ledStart; led <= ledEnd; led++) {
-    digitalWrite(led, bitRead(hour, led - ledStart)); 
+    digitalWrite(led, bitRead(number, led - ledStart)); 
   }
 }
 
 // Show visual confirmation of setting the time
 void showHourActive() {
   showActive(led9, led13);
+  writeHour();
 }
 
 void showMinuteActive() {
   showActive(led3, led8);
+  writeMinute();
+}
+
+void showSetDone() {
+  for (int i = 0; i < 3; i++) {
+    writeTime(0, led3, led13);
+    delay(200);
+    writeHour();
+    writeMinute();
+    delay(200); 
+  }
 }
 
 void showActive(int ledStart, int ledEnd) {
